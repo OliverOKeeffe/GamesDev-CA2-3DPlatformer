0a71bd119ece4e6b0debf228bcc839594a90acb9
OliverOKeeffe <125363675+OliverOKeeffe@users.noreply.github.com>
2024-12-09 20:34:15 +0000

    Created a system for taking damage and reseting the scene once you die, and got enemy to follow Abe

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,29 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; // Required for scene management
 
 public class PlayerHealth : MonoBehaviour
 {
     public int currentHealth, maxHealth, DamageAmmount;
-    // Start is called before the first frame update
+    public Healthbar healthbar;
+
     void Start()
     {
         currentHealth = maxHealth;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        healthbar?.SetMaxHealth(currentHealth); // Initialize health bar
     }
 
     public void DealDamage()
     {
         currentHealth -= DamageAmmount;
+        Debug.Log("Damage dealt. Current health: " + currentHealth);
+
+        healthbar?.SetHealth(currentHealth); // Update health bar
+
         if (currentHealth <= 0)
         {
-            gameObject.SetActive(false);
+            ResetScene();
         }
-        
+    }
+
+    private void ResetScene()
+    {
+        Debug.Log("Player health reached zero. Resetting scene...");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // Reload the current scene
     }
 }
